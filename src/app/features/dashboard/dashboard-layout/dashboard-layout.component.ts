b0d829c157266a153b2daf99b1ce1095bfcd42fa
kxgv <guinakevin@gmail.com>
2025-02-13 21:34:49 +0100

    created dashboard-layout component

--- src/app/features/dashboard/dashboard-layout/dashboard-layout.component.ts
+++ src/app/features/dashboard/dashboard-layout/dashboard-layout.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ProductsHomeListComponent } from '../../products/products-home-list/products-home-list/products-home-list.component';
 
 @Component({
   selector: 'app-dashboard-layout',
-  imports: [],
+  imports: [ProductsHomeListComponent],
   templateUrl: './dashboard-layout.component.html',
   styleUrl: './dashboard-layout.component.css'
 })
